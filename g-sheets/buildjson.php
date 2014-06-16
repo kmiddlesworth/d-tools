6bd5948146682faca7297133eed3585c228ead28
kmiddlesworth <middlesworth-2@Kevins-MacBook-Pro.local>
2014-06-15 21:07:01 -0500

    php error fix

--- g-sheets/buildjson.php
+++ g-sheets/buildjson.php
@@ -4,6 +4,7 @@ echo '<h1>Google Sheet Data</h1><h2>Key: ' . $_POST['key'] . '</h2>';
     
 //echo $_POST['input'];
 
-file_put_contents('json-output/' . $_POST['key'] . '.js', $_POST['input']);
+file_put_contents('json-output-js/' . $_POST['key'] . '.js', $_POST['input']);
+file_put_contents('json-output-php/' . $_POST['key'] . '.js', JSON_encode($_POST['input']));
 
 ?>
--- g-sheets/index.php
+++ g-sheets/index.php
@@ -67,7 +67,15 @@
         <![endif]-->
         
         <div id="wrapper">
-             
+        
+            <? if (!isset($_GET['gkey'])): ?>
+            
+            <h1>Google Sheets</h1>
+            
+            
+            
+            <? endif; ?>          
+        
         </div>
     
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -92,7 +100,8 @@
                                                 
                         var dataString = '<p><strong>Compiled JSON:</strong></p><pre>' + JSON.stringify(gSheetData, null, 4) + '</pre>',
                             projectBtn = '<a class="button" href="' + gSheetData.sheets.config[0].projecturl + '">Project: ' + gSheetData.sheets.config[0].projectname + '</a>',
-                            phpIncludeCode = '<p><strong>PHP Include:</strong></p><pre>&lt;script&gt;&lt;?= file_get_contents("/d-tools/g-sheets/json-output/' + gSheetData.key + '.js") ?&gt;&lt;/script&gt;</pre>';
+                            phpIncludeCode = '<p><strong>PHP Include:</strong></p><pre>&lt;?= include("/d-tools/g-sheets/json-output-php/' + gSheetData.key + '.php") ?&gt;</pre>',
+                            phpJsIncludeCode = '<p><strong>PHP JS head Include:</strong></p><pre>&lt;script&gt;&lt;?= var gSheetData = file_get_contents($_SERVER[\'DOCUMENT_ROOT\'] . "/d-tools/g-sheets/json-output-js/' + gSheetData.key + '.js") ?&gt;&lt;/script&gt;</pre>';
                         
                         document.getElementById("wrapper").innerHTML = d + projectBtn + phpIncludeCode + dataString;
                     
